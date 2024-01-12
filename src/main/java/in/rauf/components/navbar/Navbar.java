0866ae8fb10b2710ff23e3949c4979cb8a6c6db9
Abdul Rauf <abdul3rauf@gmail.com>
2024-01-12 14:38:48 +0530

    add navbar

---
+++ src/main/java/in/rauf/components/navbar/Navbar.java
@@ -0,0 +1,29 @@
+package in.rauf.components.navbar;
+
+
+import in.rauf.pages.about.AboutPage;
+import in.rauf.pages.home.HomePage;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class Navbar extends Panel {
+
+    public Navbar(String id) {
+        super(id);
+
+        add(new Link<Void>("homePageLink") {
+            @Override
+            public void onClick() {
+                setResponsePage(HomePage.class);
+            }
+        });
+
+        add(new Link<Void>("aboutPageLink") {
+            @Override
+            public void onClick() {
+                setResponsePage(AboutPage.class);
+            }
+        });
+
+    }
+}
