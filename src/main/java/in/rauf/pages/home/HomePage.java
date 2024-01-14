28524eab98084e24aedb4ce7b5c081e8d2f142d7
Abdul Rauf <abdul3rauf@gmail.com>
2024-01-15 02:59:32 +0530

    add home page content

--- src/main/java/in/rauf/pages/home/HomePage.java
+++ src/main/java/in/rauf/pages/home/HomePage.java
@@ -1,11 +1,21 @@
 package in.rauf.pages.home;
 
 import in.rauf.layouts.main.MainLayout;
+import in.rauf.pages.task.TaskManagementPage;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class HomePage extends MainLayout {
 
+    public static final String REDIRECT_TO_TASK_PAGE_LINK = "redirectToTaskPageLink";
+
     public HomePage(final PageParameters parameters) {
         super();
+        add(new Link<Void>(REDIRECT_TO_TASK_PAGE_LINK) {
+            @Override
+            public void onClick() {
+                setResponsePage(TaskManagementPage.class);
+            }
+        });
     }
 }
