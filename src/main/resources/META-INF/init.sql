a2f3ffdcc9f304bd13bb3a1e167e088ee2c81494
Abdul Rauf <abdul3rauf@gmail.com>
2024-01-15 14:59:55 +0530

    update README

--- src/main/resources/META-INF/init.sql
+++ src/main/resources/META-INF/init.sql
@@ -5,7 +5,7 @@ INSERT INTO properties(id, name, created_at, updated_at) VALUES (4, 'Dubai Mall'
 
 INSERT INTO users(id, name, email, created_at, updated_at) VALUES (1, 'Abdul', 'abdul@test.com', now(), now());
 INSERT INTO users(id, name, email, created_at, updated_at) VALUES (2, 'Robert', 'robert@test.com', now(), now());
-INSERT INTO users(id, name, email, created_at, updated_at) VALUES (3, 'Sylvain', 'sylvain@test.com', now(), now());
+INSERT INTO users(id, name, email, created_at, updated_at) VALUES (3, 'David', 'david@test.com', now(), now());
 
 INSERT INTO tasks (id, name, priority, status, assigned_to, due_date, created_at, updated_at, property_id) VALUES (1, 'Cleaning the facade', 'MEDIUM', 'TODO', 1, '2024-01-01', now(), now(), 1);
 INSERT INTO tasks (id, name, priority, status, assigned_to, due_date, created_at, updated_at, property_id) VALUES (2, 'Renewing the fire extinguishers', 'HIGH', 'COMPLETED', 2, '2024-01-03', now(), now(), 1);
